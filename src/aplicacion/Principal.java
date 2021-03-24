7c3385b664f640ac4ba494dacf9640c5d0b80032
Martin <martin.berlanasmolera@usp.ceu.es>
2021-03-24 13:57:51 +0100

    Quitadas tildes

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -7,12 +7,12 @@ public class Principal {
         try {
 
             Tablero tablero = new Tablero();
-            System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
+            System.out.println("SIMULACION CON TABLERO LEIDO");
             tablero.leerEstadoActual();
             System.out.println(tablero);
 
             for(int i = 0; i <= 5; i++) {
-                
+
                 TimeUnit.SECONDS.sleep(1);
                 tablero.transitarAlEstadoSiguiente();
                 System.out.println(tablero);
